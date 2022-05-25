394c9c08613c6dfb49f3129f60840e9d4971a67e
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-05-25 12:12:41 -0700

    Add modal js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-const elLogoLink = document.querySelector('.sitenav-link');
+const elModal =document.querySelector('.modal');
+const elModalClose = elModal.querySelector('.js-modal-close');
+
+if (elModalClose) {
+  elModalClose.addEventListener('click',function () {
+    elModal.classList.remove('modal__open');
+  });
+}
+
+setTimeout (function () {
+  elModal.classList.add('modal__open');
+}, 3000);
