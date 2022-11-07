35fcb11c80b4235389ca0a303fb7374c31e7ea1e
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-11-07 14:16:34 +0500

    add modal close

--- js/main.js
+++ js/main.js
@@ -1,24 +1,43 @@
 // MODAL
-const elModal =document.querySelector('.modal');
+const elModal = document.querySelector('.modal');
 const elModalClose = elModal.querySelector('.js-modal-close');
+const modaClose = document.querySelector('.modal__close-icon')
+let elQuizesTop = document.querySelectorAll('.preferential-quizes__top');
+let elQuizesList = document.querySelectorAll('.preferential-quizes__list');
+const elCloseBtn = document.querySelector('.close-btn')
+
+elCloseBtn.addEventListener('click', () => {
+  elModal.classList.add('hidden')
+})
+
+elModal.addEventListener('click', () => {
+  elModal.classList.add('hidden')
+})
+
+document.addEventListener('keydown', (e) => {
+  if (e.key == 'Escape') {
+    elModal.classList.add('hidden')
+  }
+})
 
 if (elModalClose) {
-  elModalClose.addEventListener('click',function () {
+  elModalClose.addEventListener('click', function () {
     elModal.classList.remove('modal__open');
   });
 }
 
-setTimeout (function () {
+setTimeout(function () {
   elModal.classList.add('modal__open');
-}, 30000);
+}, 300);
 // /Modal
 
 
-let elQuizesTop = document.querySelectorAll('.preferential-quizes__top');
-let elQuizesList = document.querySelectorAll('.preferential-quizes__list');
+elQuizesTop.forEach(function (item, index) {
+  item.addEventListener('click', function () {
+    elQuizesList[index].classList.toggle('preferential-quizes-js')
+  })
+})
 
-elQuizesTop.forEach(function(item , index) {
-    item.addEventListener('click', function() {
-      elQuizesList[index].classList.toggle('preferential-quizes-js')
-    })
+document.addEventListener('keydown', (e) => {
+  if (e, key == 'Escape') { }
 })
